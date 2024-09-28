00c0d19cce1ef9add6a42383db7c68f278847da8
Markus Nyman <markus@nyman.dev>
2024-09-28 22:23:33 +0300

    Change tinymist to typst-lsp

--- lua/plugins/lsp/config.lua
+++ lua/plugins/lsp/config.lua
@@ -89,7 +89,7 @@ function M.setup()
     lsp.clangd.setup(default_settings)
 
     lsp.ltex.setup(default_settings)
-    lsp.tinymist.setup(vim.tbl_extend("force", default_settings, { root_dir = lsp.util.root_pattern("main.typ") }))
+    lsp.typst_lsp.setup(vim.tbl_extend("force", default_settings, { root_dir = lsp.util.root_pattern("main.typ") }))
 
     vim.g.markdown_fenced_languages = {
         'ts=typescript'
