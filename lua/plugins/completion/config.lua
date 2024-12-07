3cf8108d4fcfbe8c8a0e1819fc2c125214b7f7e5
Markus Nyman <markus@nyman.dev>
2024-12-07 19:43:36 +0200

    Enable LuaSnip

--- lua/plugins/completion/config.lua
+++ lua/plugins/completion/config.lua
@@ -1,102 +1,29 @@
-local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-local cmp = require("cmp")
-local compare = require("cmp.config.compare")
-local luasnip = require("luasnip")
-local lspkind = require("lspkind")
+return {
+    keymap = { preset = 'default' },
 
-require("luasnip.loaders.from_vscode").lazy_load()
-require("luasnip.loaders.from_vscode").lazy_load({ paths = "./snippets" })
-
-local source_mapping = {
-    buffer = "[Buffer]",
-    nvim_lsp = "[LSP]",
-    luasnip = "[LuaSnip]",
-    nvim_lua = "[nvim]",
-    path = "[Path]",
-    npm = "[NPM]",
-    latex = "[LaTeX]",
-}
-
-cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
+    appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
     },
-    mapping = {
-        ["<C-j>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_next_item { behavior = cmp.SelectBehavior.Insert }
-            elseif luasnip.jumpable(1) then
-                luasnip.jump(1)
-            end
-        end),
-        ["<C-k>"] = cmp.mapping(function()
-            if cmp.visible() then
-                cmp.select_prev_item { behavior = cmp.SelectBehavior.Insert }
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            end
-        end),
-        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.close(),
-        ["<CR>"] = cmp.mapping.confirm { select = true },
-    },
+
     sources = {
-        { name = "npm",      priority = 9, keyword_length = 4 },
-        { name = "nvim_lsp", priority = 8 },
-        { name = "lazydev",  priority = 8, group_index = 0 },
-        { name = "luasnip",  priority = 7, max_item_count = 4, option = { history = false } },
-        { name = "path",     priority = 6 },
-        { name = "buffer",   priority = 5 },
+        completion = {
+            enabled_providers = { 'lsp', 'path', 'luasnip', 'buffer' },
+        },
     },
-    sorting = {
-        priority_weight = 1.0,
-        comparators = {
-            compare.offset,
-            compare.exact,
-            compare.score,
 
-            -- copied from cmp-under, but I don't think I need the plugin for this.
-            -- I might add some more of my own.
-            function(entry1, entry2)
-                local _, entry1_under = entry1.completion_item.label:find "^_+"
-                local _, entry2_under = entry2.completion_item.label:find "^_+"
-                entry1_under = entry1_under or 0
-                entry2_under = entry2_under or 0
-                if entry1_under > entry2_under then
-                    return false
-                elseif entry1_under < entry2_under then
-                    return true
-                end
-            end,
+    snippets = {
+        expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+        active = function(filter)
+            if filter and filter.direction then
+                return require('luasnip').jumpable(filter.direction)
+            end
+            return require('luasnip').in_snippet()
+        end,
+        jump = function(direction) require('luasnip').jump(direction) end,
+    },
 
-            compare.kind,
-            compare.sort_text,
-            compare.length,
-            compare.order,
-        },
-    },
-    formatting = {
-        format = lspkind.cmp_format({
-            mode = "symbol_text",
-            menu = source_mapping,
-        })
-    },
-    preselect = cmp.PreselectMode.None,
-    window = {
-        completion = cmp.config.window.bordered {
-            winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
-        },
-        documentation = cmp.config.window.bordered {
-            winhighlight = 'Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None',
-        },
-    },
-    performance = {
-        max_view_entries = 50,
-    },
+    completion = { accept = { auto_brackets = { enabled = true } } },
+
+    signature = { enabled = true }
 }
-
-cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -1,35 +1,10 @@
 return {
     'saghen/blink.cmp',
     lazy = false,
-    dependencies = 'rafamadriz/friendly-snippets',
-
-    version = 'v0.*',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        -- 'default' for mappings similar to built-in completion
-        -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-        -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-        -- see the "default configuration" section below for full documentation on how to define
-        -- your own keymap.
-        keymap = { preset = 'default' },
-
-        appearance = {
-            use_nvim_cmp_as_default = true,
-            nerd_font_variant = 'mono'
-        },
-
-        -- default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, via `opts_extend`
-        sources = {
-            completion = {
-                enabled_providers = { 'lsp', 'path', 'snippets', 'buffer' },
-            },
-        },
-
-        completion = { accept = { auto_brackets = { enabled = true } } },
-
-        signature = { enabled = true }
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'L3MON4D3/LuaSnip'
     },
+    version = 'v0.*',
+    opts = require("plugins.completion.config"),
 }
