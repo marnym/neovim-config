5cb83ad8e69a21dbaedc088ad30c036c3a555761
Markus Nyman <markus@nyman.dev>
2024-12-07 23:15:23 +0200

    Add .editorconfig

--- init.lua
+++ init.lua
@@ -1,31 +1,31 @@
-require("opts")
+require('opts')
 
 -- load lazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
-    vim.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
+    vim.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
         lazypath,
-    })
+    }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins", {
+require('lazy').setup('plugins', {
     -- would be nice to not hardcode this
-    lockfile = vim.fn.expand("$HOME/neovim-config/lazy-lock.json"),
+    lockfile = vim.fn.expand('$HOME/neovim-config/lazy-lock.json'),
     install = {
         missing = false,
     },
 })
 
-vim.api.nvim_create_autocmd("User", {
-    pattern = "VeryLazy",
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'VeryLazy',
     callback = function()
-        require("binds")
-        require("globals")
-    end
+        require('binds')
+        require('globals')
+    end,
 })
