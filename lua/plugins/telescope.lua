e3342adc8a21895366d915c74d7c8817a7848f55
Markus Nyman <markus@nyman.dev>
2024-12-10 11:23:10 +0200

    Enable preview in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,3 @@
-local function no_preview()
-    return require('telescope.themes').get_dropdown {
-        borderchars = {
-            { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-            prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
-            results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
-            preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
-        },
-        previewer = false,
-        prompt_title = false,
-    }
-end
-
 return {
     'nvim-telescope/telescope.nvim',
     dependencies = {
@@ -36,53 +23,65 @@ return {
                 file_ignore_patterns = {
                     'node_modules/',
                 },
+                layout_strategy = 'vertical',
+                layout_config = {
+                    vertical = { mirror = true, prompt_position = 'top', width = 0.75, height = 0.50 },
+                },
+                borderchars = {
+                    { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+                    prompt = { '─', '│', ' ', '│', '╭', '╮', '│', '│' },
+                    results = { '─', '│', '─', '│', '├', '┤', '╯', '╰' },
+                    preview = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+                },
+                previewer = true,
+                prompt_title = false,
+            },
+            extensions = {
+                fzf = {},
             },
         }
-        pcall(require('telescope').load_extension, 'fzf')
     end,
     keys = {
-        { '<leader><space>', function() require('telescope.builtin').buffers(no_preview()) end, desc = '[ ] Find existing buffers' },
+        { '<leader><space>', function() require('telescope.builtin').buffers() end, desc = '[ ] Find existing buffers' },
         {
             '<leader>fp',
-            function() require('telescope.builtin').git_files(no_preview()) end,
+            function() require('telescope.builtin').git_files() end,
             desc = '[F]ind [P]roject files',
         },
         {
             '<leader>ff',
-            function() require('telescope.builtin').find_files(no_preview()) end,
+            function() require('telescope.builtin').find_files() end,
             desc = '[F]ind [F]iles',
         },
         {
             '<leader>sw',
-            function() require('telescope.builtin').grep_string(no_preview()) end,
+            function() require('telescope.builtin').grep_string() end,
             desc = '[S]earch current [W]ord',
         },
         {
             '<leader>fg',
-            function() require('telescope.builtin').live_grep(no_preview()) end,
+            function() require('telescope.builtin').live_grep() end,
             desc = '[F]ind by [G]rep',
         },
         {
             '<leader>sh',
-            function() require('telescope.builtin').help_tags(no_preview()) end,
+            function() require('telescope.builtin').help_tags() end,
             desc = '[S]earch [H]elp',
         },
         {
             '<leader>ss',
-            function() require('telescope.builtin').lsp_dynamic_workspace_symbols(no_preview()) end,
+            function() require('telescope.builtin').lsp_dynamic_workspace_symbols() end,
             desc = '[S]earch [S]ymbols',
         },
         {
             '<leader>sd',
-            function() require('telescope.builtin').diagnostics(no_preview()) end,
+            function() require('telescope.builtin').diagnostics() end,
             desc = '[S]earch [D]iagnostics',
         },
         {
             '<leader>ys',
-            function() require('telescope').extensions.yaml_schema.yaml_schema(no_preview()) end,
+            function() require('telescope').extensions.yaml_schema.yaml_schema() end,
             desc = '[Y]aml [S]chema',
         },
     },
-
-    no_preview = no_preview,
 }
