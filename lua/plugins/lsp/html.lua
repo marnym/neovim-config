a631e8cd9563fbba56bece8dcc34bc4481e7e94c
Markus Nyman <markus@nyman.dev>
2024-12-13 19:18:13 +0200

    Fix superhtml typo

--- lua/plugins/lsp/html.lua
+++ lua/plugins/lsp/html.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function(lsp, capabilities)
     lsp.html.setup { capabilities = capabilities }
-    lsp.superthtml.setup { capabilities = capabilities }
+    lsp.superhtml.setup { capabilities = capabilities }
     lsp.cssls.setup { capabilities = capabilities }
 end
 
