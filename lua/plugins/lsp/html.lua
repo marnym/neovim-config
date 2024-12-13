75d31783dbf59598ef1cf2757506f045161312f8
Markus Nyman <markus@nyman.dev>
2024-12-13 15:41:06 +0200

    Add superhtml

--- lua/plugins/lsp/html.lua
+++ lua/plugins/lsp/html.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.setup = function(lsp, capabilities)
     lsp.html.setup { capabilities = capabilities }
+    lsp.superthtml.setup { capabilities = capabilities }
     lsp.cssls.setup { capabilities = capabilities }
 end
 
