0506d84053025e72eb32ae89407b323f344c0257
Markus Nyman <markus@nyman.dev>
2024-12-13 20:01:17 +0200

    Load snippets

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -3,7 +3,14 @@ return {
     lazy = false,
     dependencies = {
         'rafamadriz/friendly-snippets',
-        { 'L3MON4D3/LuaSnip', version = 'v2.*' },
+        {
+            'L3MON4D3/LuaSnip',
+            version = 'v2.*',
+            config = function()
+                require('luasnip.loaders.from_vscode').lazy_load()
+                require('luasnip.loaders.from_vscode').lazy_load { paths = './snippets' }
+            end,
+        },
         {
             'folke/lazydev.nvim',
             ft = 'lua',
