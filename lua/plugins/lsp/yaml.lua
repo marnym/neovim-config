feef33782ff53ad379f90fb4c4aa2c2947542d97
Markus Nyman <markus@nyman.dev>
2025-03-14 17:41:59 +0200

    Add YamlSchema command

--- lua/plugins/lsp/yaml.lua
+++ lua/plugins/lsp/yaml.lua
@@ -1,10 +1,23 @@
 local M = {}
 
 M.setup = function(lsp, capabilities)
-    local yaml_cfg = require('yaml-companion').setup {
+    local yaml_companion = require('yaml-companion')
+    local yaml_cfg = yaml_companion.setup {
         lspconfig = { capabilities = capabilities },
     }
     lsp.yamlls.setup(yaml_cfg)
+
+    vim.api.nvim_create_user_command('YamlSchema',
+        function()
+            local fzf = require('fzf-lua')
+            fzf.register_ui_select()
+            yaml_companion.open_ui_select()
+            fzf.deregister_ui_select()
+        end,
+        {
+            desc = 'Select YAML schema',
+        }
+    )
 end
 
 return M
