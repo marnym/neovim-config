c5bfa4ac9431c5166bcc3e81b654ba7cde968a87
Markus Nyman <markus@nyman.dev>
2025-03-29 01:29:33 +0200

    Move lazy.nvim initialization to own file

---
+++ lua/core/lazy.lua
@@ -0,0 +1,34 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+            { out,                            'WarningMsg' },
+            { '\nPress any key to exit...' },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup {
+    spec = {
+        { import = 'plugins' },
+    },
+    -- would be nice to not hardcode this
+    lockfile = vim.fn.expand('$HOME/.config/nvim/lazy-lock.json'),
+    install = {
+        missing = false,
+    },
+}
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'VeryLazy',
+    callback = function()
+        require('core.binds')
+        require('core.globals')
+    end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,7 +11,6 @@ vim.api.nvim_create_autocmd('User', {
     end,
 })
 
-
 return {
     {
         'folke/snacks.nvim',
