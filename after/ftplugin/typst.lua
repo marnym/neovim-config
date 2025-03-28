9ab8a3cd47998b54a4d42088dccf81478c627f4d
Markus Nyman <markus@nyman.dev>
2025-03-28 23:55:37 +0200

    Add bind for starting typst-watch

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -4,3 +4,10 @@ vim.bo.spelllang = 'en_us'
 vim.o.spell = true
 
 vim.bo.shiftwidth = 2
+
+vim.keymap.set('n', '<leader>w', function()
+        local tw = require('typst-watch')
+        tw.watch(vim.fn.expand('%:p'))
+        tw.open_preview(nil)
+    end,
+    { noremap = true, desc = 'Start Typst [W]atch and preview' })
