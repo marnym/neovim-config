9d6930dc46f52042b745b6cbb1902467dfb4017d
Markus Nyman <markus@nyman.dev>
2025-04-12 12:53:20 +0300

    Use ansible language server through npx

--- lsp/ansiblels.lua
+++ lsp/ansiblels.lua
@@ -1,6 +1,6 @@
 --- @type vim.lsp.Config
 return {
-    cmd = { 'ansible-language-server', '--stdio' },
+    cmd = { 'npx', '@ansible/ansible-language-server', '--stdio' },
     filetypes = { 'yml', 'yaml', 'yml.ansible', 'yaml.ansible' },
     root_markers = { 'ansible.cfg' },
     single_file_support = false,
