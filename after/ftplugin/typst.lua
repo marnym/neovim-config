83dd95ed11116ff26b918c6f0959684d2f8cb7af
Markus Nyman <markus@nyman.dev>
2025-04-12 12:53:08 +0300

    Set textwidth for typst files

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -2,6 +2,8 @@ vim.bo.commentstring = '// %s'
 
 vim.bo.shiftwidth = 2
 
+vim.bo.textwidth = 120
+
 vim.keymap.set('n', '<leader>w', function()
         local tw = require('typst-watch')
         tw.watch(vim.fn.expand('%:p'))
