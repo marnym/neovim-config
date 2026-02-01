4a5622396a9d0e4d796bd2ca84a32b9b93e86503
Markus Nyman <markus@nyman.dev>
2026-02-01 19:50:55 +0200

    Configure denols root_dir

--- after/lsp/denols.lua
+++ after/lsp/denols.lua
@@ -1,18 +1,17 @@
 --- @type vim.lsp.Config
 return {
-    root_markers = { 'deno.json', 'deno.jsonc' },
-    filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
-    settings = {
-        deno = {
-            enable = true,
-            unstable = true,
-            suggest = {
-                imports = {
-                    hosts = {
-                        ['https://deno.land'] = true,
-                    },
-                },
-            },
-        },
-    },
+    root_dir = function(bufnr, on_dir)
+        local root_markers = { { 'deno.lock' }, { '.git' } }
+        -- exclude non-deno projects (npm, yarn, pnpm, bun)
+        local non_deno_path = vim.fs.root(
+            bufnr,
+            { 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
+        )
+        local project_root = vim.fs.root(bufnr, root_markers)
+        if non_deno_path and (not project_root or #non_deno_path >= #project_root) then
+            return
+        end
+        -- We fallback to the current working directory if no project root is found
+        on_dir(project_root or vim.fn.getcwd())
+    end,
 }
