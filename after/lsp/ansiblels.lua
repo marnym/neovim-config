373cc8c696c968a2869223c5cec7e1c751c2cd75
Markus Nyman <markus@nyman.dev>
2026-02-01 19:48:43 +0200

    Run ansiblels using Deno

--- after/lsp/ansiblels.lua
+++ after/lsp/ansiblels.lua
@@ -1,5 +1,15 @@
 --- @type vim.lsp.Config
 return {
-    cmd = { 'npx', '@ansible/ansible-language-server', '--stdio' },
+    cmd = { 'deno', 'run', '-A', 'npm:@ansible/ansible-language-server@1.2.3', '--stdio' },
     filetypes = { 'yml', 'yaml', 'yml.ansible', 'yaml.ansible' },
+    settings = {
+        ansible = {
+            validation = {
+                enabled = false,
+                lint = {
+                    enabled = false,
+                },
+            },
+        },
+    },
 }
