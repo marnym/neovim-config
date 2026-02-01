1324e62f21707a9144eff97b46e8b7ce5aaa795b
Markus Nyman <markus@nyman.dev>
2026-02-01 18:49:27 +0200

    Search commands to leader + single key

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -14,33 +14,33 @@ require('fzf-lua').setup {
     },
 }
 
-vim.keymap.set('n', '<leader>sh', function() require('fzf-lua').helptags() end, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>h', function() require('fzf-lua').helptags() end, { desc = 'Search [H]elp' })
 vim.keymap.set('n',
-    '<leader>ff',
+    '<leader>f',
     -- default is copied from https://github.com/ibhagwan/fzf-lua/wiki/Options
     -- node_modules is added to it
     function() require('fzf-lua').files { fd_opts = '--color=never --type f --hidden --follow --exclude .git --exclude node_modules' } end,
-    { desc = '[F]earch [F]iles' }
+    { desc = 'Search [F]iles' }
 )
 vim.keymap.set('n',
-    '<leader>fg',
+    '<leader>g',
     -- default is copied from https://github.com/ibhagwan/fzf-lua/wiki/Options
     -- node_modules is added to it
     function() require('fzf-lua').live_grep { cmd = "rg --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -g '!{.git,node_modules}/' -e" } end,
-    { desc = '[F]ind using [G]rep' }
+    { desc = 'Search using [G]rep' }
 )
 vim.keymap.set('n',
-    '<leader>fp',
+    '<leader>p',
     function() require('fzf-lua').git_files() end,
-    { desc = '[F]ind Git files' }
+    { desc = 'Search Git files' }
 )
 vim.keymap.set('n',
-    '<space><space>',
+    '<leader><space>',
     function() require('fzf-lua').buffers() end,
     { desc = 'Find buffers' }
 )
 vim.keymap.set('n',
-    '<leader>ss',
+    '<leader>s',
     function() require('fzf-lua').lsp_workspace_symbols() end,
-    { desc = '[S]earch [S]ymbols' }
+    { desc = 'Search [S]ymbols' }
 )
