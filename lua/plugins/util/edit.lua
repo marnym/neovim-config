4af0c658d4552e00b5a2ef822bc0ddda340a5649
Markus Nyman <markus@nyman.dev>
2026-02-05 18:11:56 +0200

    plugin(edit): Add nvim-ts-autotag

--- lua/plugins/util/edit.lua
+++ lua/plugins/util/edit.lua
@@ -1,6 +1,7 @@
 vim.pack.add {
     'https://github.com/nvim-mini/mini.surround',
     'https://github.com/nvim-mini/mini.pairs',
+    'https://github.com/windwp/nvim-ts-autotag',
 }
 
 require('mini.surround').setup()
