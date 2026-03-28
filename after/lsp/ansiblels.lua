e71e6838065964484fad93a85b630971df051ebe
Markus Nyman <markus@nyman.dev>
2026-03-28 20:38:56 +0200

    lsp: Only start ansiblels if filetype is ansible yaml

--- after/lsp/ansiblels.lua
+++ after/lsp/ansiblels.lua
@@ -1,7 +1,7 @@
 --- @type vim.lsp.Config
 return {
     cmd = { 'deno', 'run', '-A', 'npm:@ansible/ansible-language-server@1.2.3', '--stdio' },
-    filetypes = { 'yml', 'yaml', 'yml.ansible', 'yaml.ansible' },
+    filetypes = { 'yml.ansible', 'yaml.ansible' },
     settings = {
         ansible = {
             validation = {
