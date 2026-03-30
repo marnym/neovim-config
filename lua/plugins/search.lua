3a8843f193602062860e36ef916a2fd6af6a310c
Markus Nyman <markus@nyman.dev>
2026-03-30 16:40:23 +0300

    plugin/search: Replace fzf-lua with mini.pick

--- lua/plugins/search.lua
+++ lua/plugins/search.lua
@@ -1,46 +1,72 @@
 vim.pack.add {
-    'https://github.com/ibhagwan/fzf-lua',
-    'https://github.com/nvim-tree/nvim-web-devicons',
+    'https://github.com/nvim-mini/mini.pick',
+    -- For additional pickers.
+    'https://github.com/nvim-mini/mini.extra',
 }
 
-require('fzf-lua').setup {
-    winopts = {
-        width = 0.70,
-        height = 0.75,
-        preview = {
-            layout = 'vertical',
-            vertical = 'down:65%',
-        },
+--- Centers window on screen
+--- See: https://nvim-mini.org/mini.nvim/doc/mini-pick.html#minipick-examples-differentwindowstyles
+local function win_config()
+    local height = math.floor(0.618 * vim.o.lines)
+    local width = math.floor(0.618 * vim.o.columns)
+
+    return {
+        border = 'rounded',
+        anchor = 'NW',
+        height = height,
+        width  = width,
+        row    = math.floor(0.5 * (vim.o.lines - height)),
+        col    = math.floor(0.5 * (vim.o.columns - width)),
+    }
+end
+
+require('mini.pick').setup {
+    mappings = {
+        choose_marked = '<C-CR>',
+
+        move_down = '<C-j>',
+        move_up = '<C-k>',
+
+        -- Default mapping is <C-u>, which I want to scroll_up.
+        delete_left = '',
+
+        scroll_down = '<C-d>',
+        scroll_up = '<C-u>',
+    },
+
+    window = {
+        config = win_config,
     },
 }
+require('mini.extra').setup()
 
-vim.keymap.set('n', '<leader>h', function() require('fzf-lua').helptags() end, { desc = 'Search [H]elp' })
+vim.keymap.set('n',
+    '<leader>h',
+    MiniPick.builtin.help,
+    { desc = 'Search [H]elp' }
+)
 vim.keymap.set('n',
     '<leader>f',
-    -- default is copied from https://github.com/ibhagwan/fzf-lua/wiki/Options
-    -- node_modules is added to it
-    function() require('fzf-lua').files { fd_opts = '--color=never --type f --hidden --follow --exclude .git --exclude node_modules' } end,
+    MiniPick.builtin.files,
     { desc = 'Search [F]iles' }
 )
 vim.keymap.set('n',
     '<leader>g',
-    -- default is copied from https://github.com/ibhagwan/fzf-lua/wiki/Options
-    -- node_modules is added to it
-    function() require('fzf-lua').live_grep { cmd = "rg --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -g '!{.git,node_modules}/' -e" } end,
+    MiniPick.builtin.grep_live,
     { desc = 'Search using [G]rep' }
 )
 vim.keymap.set('n',
     '<leader>p',
-    function() require('fzf-lua').git_files() end,
+    MiniExtra.pickers.git_files,
     { desc = 'Search Git files' }
 )
 vim.keymap.set('n',
     '<leader><space>',
-    function() require('fzf-lua').buffers() end,
+    MiniPick.builtin.buffers,
     { desc = 'Find buffers' }
 )
 vim.keymap.set('n',
     '<leader>s',
-    function() require('fzf-lua').lsp_workspace_symbols() end,
+    function() MiniExtra.pickers.lsp { scope = 'workspace_symbol_live' } end,
     { desc = 'Search [S]ymbols' }
 )
