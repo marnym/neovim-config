38d3c190748c91351bae6dcc76cce8d7778eefc1
Markus Nyman <markus@nyman.dev>
2026-07-13 00:17:52 +0300

    Set diagnostic icons

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -41,6 +41,19 @@ vim.o.showmode = false
 -- keep signcolumn on
 vim.wo.signcolumn = 'yes'
 
+-- show replacement in split window
+vim.o.icm = 'split'
+
+-- setup some diagnostic icons
+vim.fn.sign_define('DiagnosticSignError',
+    { text = '', hl = 'DiagnosticSignError', texthl = 'DiagnosticSignError', culhl = 'DiagnosticSignErrorLine' })
+vim.fn.sign_define('DiagnosticSignWarn',
+    { text = '', hl = 'DiagnosticSignWarn', texthl = 'DiagnosticSignWarn', culhl = 'DiagnosticSignWarnLine' })
+vim.fn.sign_define('DiagnosticSignInfo',
+    { text = '', hl = 'DiagnosticSignInfo', texthl = 'DiagnosticSignInfo', culhl = 'DiagnosticSignInfoLine' })
+vim.fn.sign_define('DiagnosticSignHint',
+    { text = '', hl = 'DiagnosticSignHint', texthl = 'DiagnosticSignHint', culhl = 'DiagnosticSignHintLine' })
+
 -- Time in milliseconds of inactivity before calling CursorHold or writing to swap.
 vim.o.updatetime = 4000
 
